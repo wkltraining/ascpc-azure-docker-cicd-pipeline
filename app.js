e4a5ef12b1bf80c931e2ba807bd5d0305ce3219d
Raunak <54508518+TROUBLE-1@users.noreply.github.com>
2026-04-30 18:10:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Welcome to your ASCPC web app!')
+  res.send('Welcome to your ASCPC web app.')
 })
 
 app.listen(port, () => {
